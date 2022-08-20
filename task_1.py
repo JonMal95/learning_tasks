0fb3b0ea1248d34e26989aa648325da2faa0c3ba
Ivan Malgin <malgin1235@gmail.com>
2022-08-20 23:15:29 +0300

    + Initial commit

---
+++ task_1.py
@@ -0,0 +1,53 @@
+# a=3
+# a+=4
+# print(a)
+# print(2*a)
+
+# name = input('Введите ваше имя: ')
+# print('Hello', name)
+# a = int(input('Введите ваш возвраст: '))
+# print (a * 2)
+# a = int(input('Введите ваш возвраст: '))
+# b = int(input('Введите ваш возвраст: '))
+# print(a+b)
+
+#x = int(input())
+# y = int(input())
+# a = float(60*(x+y))
+# print(a)
+
+
+# all_minutes = int(input())
+#
+# hours = all_minutes//60
+# minutes = all_minutes%60
+#
+# print(f'{hours}\n{minutes}')
+
+
+
+# all_minutes = int(input()) #кол-во минут сна Кати
+# sleep_h = int(input()) #во столько часов Катя легла
+# sleep_m = int(input()) #во столько минут Катя легла
+#
+# sleep_h_m = sleep_h*60 + sleep_m + all_minutes # во столько минут Катя встала
+#
+# hours = sleep_h_m//60
+# minutes = sleep_h_m%60
+# print(hours)
+# print(minutes)
+
+# Логические значения
+# a = int(input())
+# print(a > 0)
+# a = int(input())
+# print(a >= 10 and a < 100 )
+# x1,x2,x3 = False,True, False
+# print ((not x1  or x2) and x3)
+# x = 5
+# y = 10
+# print ( y > x * x or y >= 2 * x and x < y)
+
+a = True
+b = False
+print (a and b) or (not a and not b)
